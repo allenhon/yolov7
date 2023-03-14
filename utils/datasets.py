0597c31c0ba4006faac8e5bdc6f7faf4e40f4af2
Allen <chenghon@amd.com>
2023-03-14 15:59:52 +0800

    loop through dataset.labels

--- utils/datasets.py
+++ utils/datasets.py
@@ -95,6 +95,11 @@ def create_dataloader(path, imgsz, batch_size, stride, opt, hyp=None, augment=Fa
     print ("labels_to_class_weights_labels dataset.py:",len(labels))
     sample_weights=[0]*len(labels)
     print (dataset.labels)
+    for i in dataset.labels:
+        if i[0] == 0:
+            print ("first index 0")
+        else:
+            print ('first index 1')
 
     # for idx, label in enumerate(dataset):
     #     class_weight=weights[label]
