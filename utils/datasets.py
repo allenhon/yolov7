b1a9d9fbfa6310424d532133ed7320557a304ff1
Allen <chenghon@amd.com>
2023-03-14 10:40:54 +0800

    revert cls_pw, add labels to datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -74,6 +74,23 @@ def create_dataloader(path, imgsz, batch_size, stride, opt, hyp=None, augment=Fa
                                       pad=pad,
                                       image_weights=image_weights,
                                       prefix=prefix)
+    
+    labels = np.concatenate(dataset.labels, 0)  # labels.shape = (866643, 5) for COCO
+    classes = labels[:, 0].astype(np.int)  # labels = [class xywh]
+    weights = np.bincount(classes, minlength=2)  # occurrences per class
+
+    # Prepend gridpoint count (for uCE training)
+    # gpi = ((320 / 32 * np.array([1, 2, 4])) ** 2 * 3).sum()  # gridpoints per image
+    # weights = np.hstack([gpi * len(labels)  - weights.sum() * 9, weights * 9]) ** 0.5  # prepend gridpoints to start
+
+    weights[weights == 0] = 1  # replace empty bins with 1
+    weights = 1 / weights  # number of targets per class
+    weights /= weights.sum()  # normalize
+    print ("classes from dataset.py",classes)
+    print ("labels_to_class_weights dataset.py:",weights)
+    print ("labels_to_class_weights_labels dataset.py:",labels[0])
+    print ("labels_to_class_weights_labels dataset.py:",len(labels))
+    
     filtered=len(list(filter(lambda item: item.shape[0]>0, dataset.labels)))
 
     percent = filtered / len(dataset.labels)
