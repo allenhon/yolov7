aecaa8e65430167d852e048b2f2df1aa1796d2f0
Allen <chenghon@amd.com>
2023-03-15 09:57:52 +0800

    convert classes to str

--- utils/datasets.py
+++ utils/datasets.py
@@ -97,7 +97,7 @@ def create_dataloader(path, imgsz, batch_size, stride, opt, hyp=None, augment=Fa
     print ("labels_to_class_weights_labels dataset.py:",labels[0])
     print ("labels_to_class_weights_labels dataset.py:",len(labels))
 
-    temp_class=list(classes)
+    temp_class=list(str(classes))
     print (temp_class)
     mlb_class=MultiLabelBinarizer()
     mlb_class=mlb_class.fit_transform([temp_class])
