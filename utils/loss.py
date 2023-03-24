a8ebf0adf6a4618dd9260189f2cfaf038f869846
Allen <chenghon@amd.com>
2023-03-24 09:14:38 +0800

    Change ComputeLoss as Well

--- utils/loss.py
+++ utils/loss.py
@@ -427,7 +427,8 @@ class ComputeLoss:
         h = model.hyp  # hyperparameters
         print ('ComputeLoss Triggered')
         # Define criteria
-        BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
+        # BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
+        BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([0.00018091,0.99982], device=device))
         BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['obj_pw']], device=device))
 
         # Class label smoothing https://arxiv.org/pdf/1902.04103.pdf eqn 3
@@ -563,7 +564,7 @@ class ComputeLossOTA:
 
         # Define criteria
         # BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
-        BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([0.99982,0.00018091], device=device))
+        BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([0.00018091,0.99982], device=device))
         print("torch.tensor cls_pw:",torch.tensor([0.00018091,0.99982], device=device))
         print ("BCEcls:",BCEcls)
         BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['obj_pw']], device=device))
